54dba481bd6fb7b1f1c2847f2725066b4254012f
jage <jage>
2007-04-02 07:18:03 +0000

    Make the collector code a bit cuter, the ERB-varibles are now instance variables.

--- ftp_status.rb
+++ ftp_status.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# $Id: ftp_status.rb,v 1.4 2007/03/29 08:27:27 jage Exp $
+# $Id: ftp_status.rb,v 1.5 2007/04/02 07:18:03 jage Exp $
 #
 # Written by Johan Eckerström <johan@jage.se>
 
@@ -11,32 +11,37 @@ cache_file   = "/var/tmp/ftp_status_for_#{`whoami`.strip}.cache"
 erb_template = '/opt/templates/ftp_starkast_net.erb'
 html_output  = '/var/www/ftp/index.html'
 
-# This should not be cached
-load_avg = `sysctl vm.loadavg`.split('=').last.split(' ').join(', ')
+# Use instance variables in the ERB-code
+# c[:data_size] will be expanded to @data_size
 
-d = {}
+# This should not be cached
+@load_avg = `sysctl vm.loadavg`.split('=').last.split(' ').join(', ')
+
+c = {}
 if not File.exist?(cache_file) or ((Time.now.to_i - File.new(cache_file).mtime.to_i) > cache_time)
   # Data that will be cached between runs
-  d[:data_size]    = `du -sh /var/www/ftp`.split("\t").first.gsub('G', ' GiB')
-  d[:total_files]  = `find /var/www/ftp -type f|wc -l`.to_i
-  d[:mirror_syncs] = Dir.glob('/var/log/mirror.*').collect do |log_file|
+  c[:data_size]    = `du -sh /var/www/ftp`.split("\t").first.gsub('G', ' GiB')
+  c[:total_files]  = `find /var/www/ftp -type f|wc -l`.to_i
+  c[:mirror_syncs] = Dir.glob('/var/log/mirror.*').collect do |log_file|
     s = `tail -n 1 #{log_file}`.split(',')
-    { :status => s[0],
-      :path => s[1],
-      :host => s[2],
+    { :status     => s[0],
+      :path       => s[1],
+      :host       => s[2],
       :updated_on => s[3] }
   end
   # Save Cache
   File.open(cache_file, 'w') do |cache_f|
-    cache_f.print Marshal.dump(d)
+    cache_f.print Marshal.dump(c)
   end
 else
   # Load Cache
-  d = Marshal.load(File.open(cache_file) {|fp| fp.read })
+  c = Marshal.load(File.open(cache_file) {|fp| fp.read })
 end
 
-# This should be made nicer
-data_size, total_files, mirror_syncs = d[:data_size], d[:total_files], d[:mirror_syncs]
+# Create instance variables from the c hash
+c.each_pair do |key, value|
+  self.instance_variable_set "@#{key.to_s}", value
+end
 
 # Parse ERB and write HTML
 File.open(html_output, 'w') do |html_f|
