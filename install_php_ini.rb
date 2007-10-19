16283e7cd168b046e8ff36a784f577947d447d2b
jage <jage>
2007-10-19 18:55:50 +0000

    Utility to install php.ini files for users

---
+++ install_php_ini.rb
@@ -0,0 +1,30 @@
+#!/usr/local/bin/ruby
+
+require 'erb'
+require 'optparse'
+
+$user = ''
+ARGV << '-h' if ARGV.empty?
+ARGV.options do |opts|
+  opts.on('-u', '--user USER', 'Install config for USER', String) {|$user|}
+  opts.on_tail('-h', '--help', 'Show this message') do
+    puts opts
+    exit
+  end
+  opts.parse!
+end
+
+exit 1 if $user == ''
+
+$template = '/opt/templates/php.ini.erb'
+$path = "/var/www/users/#{$user}/etc/php.ini"
+
+begin
+  raise 'User already has a php.ini!' if File.exist?($path)
+  File.open($path, 'w') do |fp|
+    fp.print ERB.new(File.read($template), nil, '>').result(binding)
+  end
+rescue => e
+  $stderr.puts e
+  exit 1
+end
