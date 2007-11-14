d8c948316736b32db3efe34c91a2f9caefb53daf
jage <jage>
2007-11-14 21:43:03 +0000

    Find site domains with wrong ips

---
+++ domain_check.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require '/opt/lib/phoo.rb'
+require 'rubygems'
+require 'net/dns/resolver'
+
+starkast_hosts = %w(
+  beaver.starkast.net
+  genau.starkast.net
+  phoo1.starkast.net
+  phoo2.starkast.net
+  phoo3.starkast.net
+  phoo4.starkast.net
+)
+sites = Marshal.load(File.read('/var/www/db/sites')).collect {|key, site| key }
+
+def resolv(sites)
+  threads = []
+  resolved = {}
+  for site in sites do
+    threads << Thread.new(site) do |domain|
+      res = Net::DNS::Resolver.new
+      res.query(domain, Net::DNS::A).each_address do |ip|
+        resolved[domain] = ip
+      end
+    end
+  end
+
+  threads.each {|t| t.join }
+  return resolved
+end
+
+starkast_ips = resolv(starkast_hosts).collect {|domain, ip| ip }
+resolv(sites).each do |domain, ip|
+  unless starkast_ips.include? ip
+    $stderr.puts "NOT OK: #{domain} => #{ip}"
+  end
+end
