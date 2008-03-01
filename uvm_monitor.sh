974d72307b0517d88eea2e70047ec5f912f0b6f4
jage <jage>
2008-03-01 17:22:29 +0000

    Restart for $max_uvm / 1.3 instead of $max_uvm / 2

--- uvm_monitor.sh
+++ uvm_monitor.sh
@@ -5,7 +5,7 @@ uvm_data=`vmstat -m | grep "    UVM amap" | sed 's/K/ /g'`
 max_uvm=`echo $uvm_data | awk '{ print $5 }'`
 cur_uvm=`echo $uvm_data | awk '{ print $3 }'`
 
-if [ "$cur_uvm" -ge "$(($max_uvm/2))" ]; then
+if [ "$cur_uvm" -ge `echo "${max_uvm}/1.3"|bc` ]; then
 	restart_text=`/bin/sh /opt/apache_restart.sh 2>&1`
 	echo "UVM amap value: ${cur_uvm} at `date` caused a restart\n\nApache output:\n${restart_text}" | mail -s "Phoo: Apache restarted by UVM monitor" root
 fi
